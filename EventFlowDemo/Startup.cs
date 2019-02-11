41b170525b119cfac0c8f03229e32ce198598804
Johnny Chan <jcwc1@hotmail.com>
2019-02-11 12:22:59 +0000

    initial event flow configuration check-in

---
+++ EventFlowDemo/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using EventFlow;
+using EventFlow.AspNetCore.Extensions;
+using EventFlow.AspNetCore.Middlewares;
+using EventFlow.Autofac.Extensions;
+using EventFlow.EventStores.Files;
+using EventFlow.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventFlowDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var containerBuilder = new ContainerBuilder();
+
+            var container = EventFlowOptions.New
+                .UseAutofacContainerBuilder(containerBuilder)
+                .AddAspNetCoreMetadataProviders()
+                .UseConsoleLog()
+                .UseFilesEventStore(FilesEventStoreConfiguration.Create("./evt-store"));
+
+            containerBuilder.Populate(services);
+
+            return new AutofacServiceProvider(containerBuilder.Build());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMiddleware<CommandPublishMiddleware>();
+            app.UseMvc();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# EventFlowDemo
+EventFlowDemo
 
-This is an my attempt to setup EventFlow in a .net core 2.2 environment due to the lack of examples available.
+An example of how to setup eventflow in .net core
